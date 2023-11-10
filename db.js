4d83dda5d491a98190062b829bc7ee88815c91da
Dereck Rojas <dereckrm2000@gmail.com>
2023-11-10 15:30:56 -0600

    Documentacion Patron Singleton

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
+// Instancia única de la conexión a la base de datos.
 let connectionInstance;
 
+/**
+ * Conecta a la base de datos MongoDB utilizando Mongoose.
+ * Este método implementa el patrón Singleton para asegurar que solo exista
+ * una instancia de la conexión a la base de datos en todo momento.
+ * 
+ * @returns {Promise} Una promesa que resuelve a la instancia de la conexión a la base de datos.
+ */
 async function connectDB() {
+    // Verifica si ya existe una instancia de la conexión.
     if (!connectionInstance) {
         try {
+            // Si no existe, crea una nueva conexión.
             connectionInstance = await mongoose.connect('mongodb://localhost:27017/LaboratorioClase', {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
@@ -14,6 +24,7 @@ async function connectDB() {
             console.error(err);
         }
     }
+    // Devuelve la instancia existente o la recién creada.
     return connectionInstance;
 }
 
